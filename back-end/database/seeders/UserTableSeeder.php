747c8b7991ed68aa0348a2d469b1c472e7b0f2fc
Jessy <bakijessy@gmail.com>
2025-09-01 13:41:33 +0000

    feat(seeder): Refactor UserTableSeeder to seed multiple users in a single run

--- back-end/database/seeders/UserTableSeeder.php
+++ back-end/database/seeders/UserTableSeeder.php
@@ -12,13 +12,43 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::create([
-            'username' => "admin",
-            'first_name' => "Default",
-            'last_name' => "User",
-            'email' => "admin@clms.com",
-            'phone' => "0000000",
-            'password' => bcrypt("admin")
-        ]);
+        $users = [
+            [
+                'username' => "admin",
+                'first_name' => "Default",
+                'last_name' => "User",
+                'email' => "admin@clms.com",
+                'phone' => "0000000",
+                'password' => bcrypt("admin")
+            ],
+            [
+                'username' => "nathaniel",
+                'first_name' => "Nathaniel",
+                'last_name' => "S.",
+                'email' => "nathaniel@clms.com",
+                'phone' => "1111111",
+                'password' => bcrypt("nathaniel")
+            ],
+            [
+                'username' => "albert",
+                'first_name' => "Albert",
+                'last_name' => "B.",
+                'email' => "albert@clms.com",
+                'phone' => "2222222",
+                'password' => bcrypt("albert")
+            ],
+            [
+                'username' => "jessy",
+                'first_name' => "Jessy",
+                'last_name' => "Baki",
+                'email' => "jessy@clms.com",
+                'phone' => "3333333",
+                'password' => bcrypt("jessy")
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
