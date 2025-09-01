742d18f473edbc71be29a75a5ecbb5a7c7b59e78
Jessy <bakijessy@gmail.com>
2025-09-01 13:41:27 +0000

    refactor(middleware): Improve code formatting and consistency in auth functions

--- front-end/lib/middleware.js
+++ front-end/lib/middleware.js
@@ -1,82 +1,78 @@
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
 
-export default async function auth({req, res}) {
-    const user = req.session.get('user')
+export default async function auth({ req, res }) {
+	const user = req.session.get("user");
 
-    if (!user) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        }
-    }
+	if (!user) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		};
+	}
 
+	let configBundle = {
+		user: req.session.get("user"),
+		apiToken: req.session.get("api_token"),
+		backendUrl: process.env.NEXT_PUBLIC_BACKEND_API_HOST + "/api",
+		hostUrl: process.env.NEXT_PUBLIC_BACKEND_API_HOST,
+		authHeader: { Authorization: "Bearer " + req.session.get("api_token") },
+	};
 
-    let configBundle = {
-        user: req.session.get('user'),
-        apiToken: req.session.get('api_token'),
-        backendUrl: process.env.BACKEND_API_HOST + "/api",
-        hostUrl: process.env.BACKEND_API_HOST,
-        authHeader: {"Authorization": "Bearer " + req.session.get('api_token')}
-    }
-
-    return {
-        props: {
-            user: req.session.get('user'),
-            api_token: req.session.get('api_token'),
-            configBundle: configBundle,
-        },
-    }
+	return {
+		props: {
+			user: req.session.get("user"),
+			api_token: req.session.get("api_token"),
+			configBundle: configBundle,
+		},
+	};
 }
 
+export async function unSecureAuth({ req, res }) {
+	const user = req.session.get("user");
 
-export async function unSecureAuth({req, res}) {
-    const user = req.session.get('user')
+	if (!user) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		};
+	}
 
-    if (!user) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        }
-    }
-
-    let configBundle = {
-        user: req.session.get('user'),
-        apiToken: req.session.get('api_token'),
-        backendUrl: process.env.BACKEND_API_HOST + "/api",
-        authHeader: {"Authorization": "Bearer " + req.session.get('api_token')}
-    }
-    return {
-        props: {
-            user: req.session.get('user'),
-            apiToken: req.session.get('api_token'),
-            backendUrl: process.env.BACKEND_API_HOST + "/api",
-            configBundle: configBundle
-        },
-    }
+	let configBundle = {
+		user: req.session.get("user"),
+		apiToken: req.session.get("api_token"),
+		backendUrl: process.env.NEXT_PUBLIC_BACKEND_API_HOST + "/api",
+		authHeader: { Authorization: "Bearer " + req.session.get("api_token") },
+	};
+	return {
+		props: {
+			user: req.session.get("user"),
+			apiToken: req.session.get("api_token"),
+			backendUrl: process.env.NEXT_PUBLIC_BACKEND_API_HOST + "/api",
+			configBundle: configBundle,
+		},
+	};
 }
 
+export async function authGuard({ req, res }) {
+	const user = req.session.get("user");
 
-export async function authGuard({req, res}) {
-    const user = req.session.get('user')
+	// redirect to dashboard if user is already logged in.
+	if (user) {
+		return {
+			redirect: {
+				destination: "/dashboard",
+				permanent: false,
+			},
+		};
+	}
 
-    // redirect to dashboard if user is already logged in.
-    if (user) {
-        return {
-            redirect: {
-                destination: '/dashboard',
-                permanent: false,
-            },
-        }
-    }
-
-    return {
-        props: {
-            url: process.env.BACKEND_API_HOST
-        },
-    }
-
+	return {
+		props: {
+			url: process.env.NEXT_PUBLIC_BACKEND_API_HOST,
+		},
+	};
 }
